83b9a45415b731aa27491cb7aaae1b55577d860a
Douglas Tiburcio Rodrigues <douglas.r129@outlook.com>
2023-02-21 16:47:48 -0300

    Creação os models migrete pizzaria

---
+++ src/routes.ts
@@ -0,0 +1,6 @@
+import { Router, Request, Response } from "express";
+const router = Router();
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({ok: true})
+})
+export default router;
